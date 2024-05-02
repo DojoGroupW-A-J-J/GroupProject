57c71b470c0b80d8f2a1242a80cb6b2628f3bf81
Joshua Gordon <j.gordon9873@gmail.com>
2024-05-02 19:35:33 -0400

    details and routes created

--- music-playlist/client/src/App.jsx
+++ music-playlist/client/src/App.jsx
@@ -1,9 +1,17 @@
 import "./css/App.css"
+//import { useState } from "react"
+import Details from "./views/Details"
+import AddSong from "./views/AddSong"
+import { Routes, Route } from "react-router-dom"
 
 function App() {
 
   return (
     <>
+    <Routes>
+      <Route path='/details' element={<Details/>}/>
+      <Route path='/add' element={<AddSong/>}/>
+    </Routes>
     </>
   )
 }
--- music-playlist/client/src/views/Details.jsx
+++ music-playlist/client/src/views/Details.jsx
@@ -1,5 +1,45 @@
+import React, {useState, useEffect} from "react";
+import { Link, useParams } from 'react-router-dom'
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 const Details = () => {
-  return <></>;
+  const {id} = useParams()
+  const [song, setSong] = useState({})
+  const navigate = useNavigate()
+
+  useEffect(() =>{
+    axios.get(`http://localhost:8000/playlists/${id}`)
+        .then((res) =>{
+            setSong(res.data)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }, [])
+
+    const deleteHandler = () =>{
+      axios.delete(`http://localhost:8000/playlists/${id}`)
+      .then(() =>{
+      navigate('/')
+  })
+  .catch((err) => {
+      console.log(err)
+  })
+}
+
+  return (
+    <div>
+      <h2>
+          {song.songTitle}<br/>
+          {song.link}<br/>
+          {song.genre}<br/>
+          {song.addedBy}
+      </h2>
+      <button onClick={deleteHandler}>Delete Song</button>
+      <Link to={`/`}>Back</Link>
+      </div>
+  )
 };
 
 export default Details;
