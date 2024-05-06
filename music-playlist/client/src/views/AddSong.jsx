314387f10aa2ec1400f470c05f9aef20ff7a10b7
JosieH <josepha.e.herrmann@gmail.com>
2024-05-06 11:56:05 -0400

    add song design finished and fully works (tested)

--- music-playlist/client/src/views/AddSong.jsx
+++ music-playlist/client/src/views/AddSong.jsx
@@ -1,54 +1,123 @@
+import * as React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
+import {
+  Box,
+  createTheme,
+  ThemeProvider,
+  TextField,
+  Button,
+} from "@mui/material";
 
 const AddSong = (props) => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [songTitle, setSongTitle] = useState('');
-    const [link, setLink] = useState('');
-    const [genre, setGenre] = useState('');
-    const [addedBy, setAddedBy] = useState('');
-    const [errors, setErrors] = useState({});
+  const [songTitle, setSongTitle] = useState("");
+  const [link, setLink] = useState("");
+  const [genre, setGenre] = useState("");
+  const [addedBy, setAddedBy] = useState("");
+  const [errors, setErrors] = useState({});
 
-    const submitHandler = (event) => {
-        event.preventDefault();
-        const newSong = { songTitle, link, genre, addedBy };
-        axios.post('http://localhost:8000/api/playlists', newSong)
-            .then((res) => {
-                console.log(res);
-                navigate('/');
-            })
-            .catch((err) => {
-                console.log(err.response.data.errors);
-                setErrors(err.response.data.errors || {});
-            });
-    };
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const newSong = { songTitle, link, genre, addedBy };
+    axios
+      .post("http://localhost:8000/api/playlists", newSong)
+      .then((res) => {
+        console.log(res);
+        navigate("/");
+      })
+      .catch((err) => {
+        console.log(err.response.data.errors);
+        setErrors(err.response.data.errors || {});
+      });
+  };
 
-    return (
-        <div>
-            <h1>Add a Song!</h1>
-            <form onSubmit={submitHandler}>
-                <label htmlFor="songTitle">Song Title</label>
-                <input type="text" name="songTitle" onChange={(event) => setSongTitle(event.target.value)} value={songTitle} />
-                {errors.songTitle && <p>{errors.songTitle.message}</p>}
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+    },
+  });
 
-                <label htmlFor="link">Link</label>
-                <input type="text" name="link" onChange={(event) => setLink(event.target.value)} value={link} />
-                {errors.link && <p>{errors.link.message}</p>}
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <div>
+        <h1>Add a Song!</h1>
+        <form onSubmit={submitHandler}>
+          <Box
+            component="form"
+            sx={{
+              "& .MuiTextField-root": { m: 1, width: "25ch" },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            {/* Song Title */}
+            <div>
+              <TextField
+                required
+                id="outlined-required"
+                label="Song Title"
+                type="text"
+                value={songTitle}
+                onChange={(event) => setSongTitle(event.target.value)}
+                InputLabelProps={{ style: { color: "white" } }}
+                InputProps={{ style: { color: "white" } }}
+              />
+              {errors.songTitle && <p>{errors.songTitle.message}</p>}
+            </div>
 
-                <label htmlFor="genre">Genre</label>
-                <input type="text" name="genre" onChange={(event) => setGenre(event.target.value)} value={genre} />
-                {errors.genre && <p>{errors.genre.message}</p>}
+            {/* Link */}
+            <div>
+              <TextField
+                required
+                id="outlined-required"
+                label="Link"
+                type="text"
+                value={link}
+                onChange={(event) => setLink(event.target.value)}
+                InputLabelProps={{ style: { color: "white" } }}
+                InputProps={{ style: { color: "white" } }}
+              />
+              {errors.link && <p>{errors.link.message}</p>}
+            </div>
 
-                <label htmlFor="addedBy">Added By</label>
-                <input type="text" name="addedBy" onChange={(event) => setAddedBy(event.target.value)} value={addedBy} />
-                {errors.addedBy && <p>{errors.addedBy.message}</p>}
+            {/* Genre */}
+            <div>
+              <TextField
+                required
+                id="outlined-required"
+                label="Genre"
+                type="text"
+                value={genre}
+                onChange={(event) => setGenre(event.target.value)}
+                InputLabelProps={{ style: { color: "white" } }}
+                InputProps={{ style: { color: "white" } }}
+              />
+              {errors.genre && <p>{errors.genre.message}</p>}
+            </div>
 
-                <button>ADD</button>
-            </form>
-        </div>
-    );
+            {/* Added By */}
+            <div>
+              <TextField
+                required
+                id="outlined-required"
+                label="Added By"
+                type="text"
+                value={addedBy}
+                onChange={(event) => setAddedBy(event.target.value)}
+                InputLabelProps={{ style: { color: "white" } }}
+                InputProps={{ style: { color: "white" } }}
+              />
+              {errors.addedBy && <p>{errors.addedBy.message}</p>}
+            </div>
+          </Box>
+          <Button type="submit">ADD</Button>
+        </form>
+      </div>
+    </ThemeProvider>
+  );
 };
 
 export default AddSong;
